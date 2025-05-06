812c508d71551f68dadfe9edaa916fff5650b8a8
Rehana Sayyed <rehanasd555@gmail.com>
2025-05-06 06:06:58 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (72.13%) - LeetSync

---
+++ Basic/7-reverse-integer/reverse-integer.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int reverse(int x) {
+        int reverse=0;
+        int rem;
+        while(x!=0){
+           rem=x%10;
+           if (reverse > Integer.MAX_VALUE / 10 || reverse < Integer.MIN_VALUE / 10) {
+                return 0;
+            }
+           reverse=reverse*10+rem;
+           x/=10; 
+        }
+        return reverse;
+            
+        }
+}
